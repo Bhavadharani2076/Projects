7c445be3dcd842af92febafe252fe0f78dd4d41c
Bhavadharani2076 <bhavadhaaranignanasaraswathy@gmail.com>
2025-09-14 18:43:16 +0530

    Use product.js file

--- Website/AmazonProject/scripts/amazon.js
+++ Website/AmazonProject/scripts/amazon.js
@@ -1,38 +1,3 @@
-const products=[
-    
-    {
-        image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: {
-            stars: 4.5,
-            count:87
-        },
-        price: 1090 // entered as cents
-
-    },
-    {
-        image:'images/products/intermediate-composite-basketball.jpg',
-        name : 'Intermediate Size Basketball',
-        rating: {
-            stars: 4,
-            count: 127
-        },
-        price: 2095 // entered as cents
-
-    },
-    {
-        image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: {
-            stars: 4.5,
-            count: 56
-        },
-        price: 799 // entered as cents
-
-    }
-
-]
-
 let productsHTML='';
 
 products.forEach((product,i)=>{
